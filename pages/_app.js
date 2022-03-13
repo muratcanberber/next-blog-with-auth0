60384bb94d544788a3ddda34feb3f9f31eb370ac
Murat Can Berber <30313472+muratcanberber@users.noreply.github.com>
2022-03-13 19:15:38 +0300

    add auth

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,21 @@
 import "../styles/global.css";
 import Header from "../components/header";
+import { Auth0Provider } from "@auth0/auth0-react";
 
 function MyApp({ Component, pageProps }) {
   return (
-    <div className="antialiased text-gray-700 ">
-      <Header />
-      <main className="mt-6 ">
-        <Component {...pageProps} />
-      </main>
-    </div>
+    <Auth0Provider
+      domain="dev-md77.us.auth0.com"
+      clientId="NFjWbQUID3vF6gbNHcTmGx6KbnYTd60p"
+      redirectUri={process.env.NEXT_PUBLIC_URL}
+    >
+      <div className="antialiased text-gray-700 ">
+        <Header />
+        <main className="mt-6 ">
+          <Component {...pageProps} />
+        </main>
+      </div>
+    </Auth0Provider>
   );
 }
 
--- pages/blog/[...slug].js
+++ pages/blog/[...slug].js
@@ -1,8 +1,10 @@
 import { getMdxNode, getMdxPaths } from "next-mdx/server";
 import { useHydrate } from "next-mdx/client";
 import { mdxComponents } from "../../components/mdx-components";
+import { useAuth0 } from "@auth0/auth0-react";
 
 function PostPage({ post }) {
+  const { loginWithRedirect, isAuthenticated, user, logout } = useAuth0();
   const content = useHydrate(post, {
     components: mdxComponents,
   });
@@ -17,6 +19,30 @@ function PostPage({ post }) {
         <hr className="my-4" />
         <div className="prose">{content}</div>
       </article>
+
+      <textarea rows="3" className="border-2 rounded-xl" />
+      {isAuthenticated ? (
+        <div className="flex items-center space-x-3">
+          <button>Send</button>
+          <img src={user.picture} width={36} className="rounded-full"></img>
+          <span>{user.name}</span>
+          <button
+            className="bg-red-800 rounded-3xl p-2 m-2 text-white mt-4"
+            onClick={() =>
+              logout({ returnTo: process.env.NEXT_PUBLIC_URL + "/blog" })
+            }
+          >
+            x
+          </button>
+        </div>
+      ) : (
+        <button
+          className="bg-green-800 text-white mt-4"
+          onClick={() => loginWithRedirect()}
+        >
+          Log In
+        </button>
+      )}
     </div>
   );
 }
